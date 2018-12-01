9ab85266830fa658dc1067e2bdb47511523b1355
Pauline Machabert <pauline@vitei.com>
2018-12-01 17:21:45 +0100

    scene reorganization

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [Header("Characteristics")]
+    public float speed = 1;
+
+    private void Update()
+    {
+        transform.position += speed * Time.deltaTime;
+    }
+}
