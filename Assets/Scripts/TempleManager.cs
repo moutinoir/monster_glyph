00d53d43985401c01bf0ad3fd1cef9b3782e40e4
Pauline Machabert <pauline@vitei.com>
2018-12-01 13:20:36 +0100

    temple root organization

---
+++ Assets/Scripts/TempleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TempleManager : MonoBehaviour
+{
+    [Header("Timeline Percentage")]
+    public float timelinePercentage = 0;
+
+    [Header("Current Temple")]
+    public TempleRoot currentTemple;
+
+    private void Update()
+    {
+        if(currentTemple != null)
+        {
+            currentTemple.SetPosition(timelinePercentage);
+        }
+    }
+}
---
+++ Assets/Scripts/TempleRoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleRoot : MonoBehaviour
+{
+    [Header("Temple Dimensions")]
+    public float length = 20;
+
+    [Header("Position and Direction")]
+    public Transform startPositionAndDirection;
+
+    Vector3 position;
+    /// <summary>
+    /// percentage 0 : the temple is at its start position
+    /// percentage 1 : the temple moved all its length along its direction
+    /// </summary>
+    /// <param name="percentage">from 0 to 1</param>
+    public void SetPosition(float percentage)
+    {
+        position = startPositionAndDirection.position + length * percentage * startPositionAndDirection.forward;
+        transform.position = position;
+    }
+}
