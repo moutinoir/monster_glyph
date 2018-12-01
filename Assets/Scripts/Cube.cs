d209bdc93859576bc4fbadd3d35b828a72edfdd0
Pauline Machabert <pauline@vitei.com>
2018-12-01 17:24:20 +0100

    empty cube script

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -4,11 +4,5 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    [Header("Characteristics")]
-    public float speed = 1;
 
-    private void Update()
-    {
-        transform.position += speed * Time.deltaTime;
-    }
 }
