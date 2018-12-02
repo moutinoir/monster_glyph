67369ce2f9556c39f54a5fe2ef14a8dd9daf7b3b
Pauline Machabert <pauline@vitei.com>
2018-12-02 17:20:43 +0100

    monsters

---
+++ Assets/extend.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class extend : MonoBehaviour
+{
+    Animator animator;
+    float time = 0;
+    float random;
+    bool bad;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        random = Random.Range(3.0f, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.time - time > random)
+        {
+            bad = !bad;
+            animator.SetBool("is_badguy", bad);
+            time = Time.time;
+        }
+	}
+}
