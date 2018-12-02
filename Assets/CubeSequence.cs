0d599a1c5f3ad92d1fe192cff4b45cb0db2e7133
David Roulin <freelance@davidroulin.ch>
2018-12-02 16:26:03 +0100

    level design sequences WIP

---
+++ Assets/CubeSequence.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CubeSequence : MonoBehaviour {
+
+    public float sequenceLength;
+
+    void RecalcLength()
+    {
+        sequenceLength = 0;
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            GridBlock isItABlock = transform.GetChild(i).GetComponent<GridBlock>();
+            if (isItABlock != null)
+            {
+                if (isItABlock.transform.localPosition.z > sequenceLength)
+                {
+                    sequenceLength = isItABlock.transform.localPosition.z;
+                }
+            }
+        }
+    }
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Application.isEditor)
+        {
+            RecalcLength();
+        }
+
+	}
+}
